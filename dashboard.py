c2a9634cc98f8ee2873e95457e98612b61fdbaf5
Tiago Santos <tiagosn06@gmail.com>
2025-05-25 15:08:38 -0300

    refact: use functions to organize the code

--- dashboard.py
+++ dashboard.py
@@ -3,68 +3,73 @@ import seaborn as sns
 import streamlit as st
 import matplotlib.pyplot as plt
 
+from helpers import get_team_data, get_team_result_frequency
 
 sns.set_theme()
 st.set_page_config(layout="wide")
 
-url = "https://raw.githubusercontent.com/adaoduque/Brasileirao_Dataset/refs/heads/master/campeonato-brasileiro-full.csv"
-
-data = pd.read_csv(url)
-
-
-def get_result(winner: str, team: str) -> str:
-    if winner == team:
-        return "Vitória"
-    if winner == "-":
-        return "Empate"
-    return "Derrota"
-
-def get_team_data(data: pd.DataFrame, team: str) -> pd.DataFrame:
-    team_data = data.copy().query(f"mandante == '{team}' or visitante == '{team}'")
-    team_data["resultado"] = team_data["vencedor"].apply(get_result, team=team)
-    return team_data[["vencedor", "resultado"]]
-
-def get_team_result_frequency(team_data: pd.DataFrame) -> pd.DataFrame:
-    freq = team_data["resultado"].value_counts().reset_index()
-    freq.columns = ["Resultado", "Quantidade"]
-    return freq
+def fetch_data():
+    url = "https://raw.githubusercontent.com/adaoduque/Brasileirao_Dataset/refs/heads/master/campeonato-brasileiro-full.csv"
+    return pd.read_csv(url)
 
 
-st.title("Campeonato Brasileiro de Futebol")
+def render_title():
 
-st.dataframe(data)
+    page_title = "Campeonato Brasileiro de Futebol"
 
-teams = data["mandante"].unique()
-
-# st.write(teams)
-
-selected_team = st.selectbox(
-    "Escolha o seu time",
-    tuple(teams)
-)
-
-team_results = get_team_result_frequency(get_team_data(data, selected_team))
-
-fig, ax = plt.subplots()
-
-colors = ["limegreen", "gold", "tomato"]
-
-team_results.sort_values(by="Resultado", inplace=True, ascending=False)
-
-ax = sns.barplot(y=team_results["Quantidade"], hue=team_results["Resultado"], palette=colors)
-
-title = f"{selected_team}\nTotal de Jogos: {team_results['Quantidade'].sum()}"
-
-ax.set_title(title, fontsize=30)
-ax.set_ylabel(None)
-ax.set(yticklabels=[])
+    st.title(page_title)
 
 
-for i in ax.containers:
-  ax.bar_label(i, fontsize=20, padding=-25)
+def render_data(data):
+    st.dataframe(data)
 
-st.pyplot(fig)
 
-plt.close()
 
-# fig.savefig("output.png", bbox_inches="tight")
+
+def render_team_result(data, save_fit=False):
+    teams = data["mandante"].unique()
+
+    # st.write(teams)
+
+    selected_team = st.selectbox(
+        "Escolha o seu time",
+        tuple(teams)
+    )
+
+    team_results = get_team_result_frequency(get_team_data(data, selected_team))
+
+    fig, ax = plt.subplots()
+
+    colors = ["limegreen", "gold", "tomato"]
+
+    team_results.sort_values(by="Resultado", inplace=True, ascending=False)
+
+    ax = sns.barplot(y=team_results["Quantidade"], hue=team_results["Resultado"], palette=colors)
+
+    title = f"{selected_team}\nTotal de Jogos: {team_results['Quantidade'].sum()}"
+
+    ax.set_title(title, fontsize=30)
+    ax.set_ylabel(None)
+    ax.set(yticklabels=[])
+
+
+    for i in ax.containers:
+        ax.bar_label(i, fontsize=20, padding=-25)
+
+    st.pyplot(fig)
+
+    plt.close()
+
+    if save_fit:
+        fig.savefig("output.png", bbox_inches="tight")
+
+
+
+def main():
+    data = fetch_data()
+    render_title()
+    render_data(data)
+    render_team_result(data)
+
+if __name__ == "__main__":
+    main()
